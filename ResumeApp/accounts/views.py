60b8dd827c67782cf9d3b1fe9aa95f081c36b25a
Rorschach <rorschach@localhost.localdomain>
2021-05-02 01:43:06 +0530

    signup authentication

--- ResumeApp/accounts/views.py
+++ ResumeApp/accounts/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Login view
@@ -16,6 +17,14 @@ class ViewLogIn(LoginView):
     # Redirecting to home page
     def get_success_url(self):
         return reverse_lazy('base.html')  # for test, should contain home url
+
+    # Redirecting logged in users to home page
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return render('base.html')
+        return super(ViewLogIn, self).get(*args, **kwargs)
+
+
 
 
 # Create sign up form
@@ -45,5 +54,8 @@ def sign_up(request):
         form = CreateSignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was successfully created for '+ user)
+            return redirect('login')
     context = {'form': form}
     return render(request, 'accounts/signup/SignUp.html', context)
